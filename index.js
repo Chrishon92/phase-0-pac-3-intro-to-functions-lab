4722118dd3bd077e27ff3efd79e24cc8faad9aa1
ChrishonCrum <chrishonkcrum@yahoo.com>
2022-03-15 21:00:57 -0500

    commit tree 833922ecd8611f743324ca05c5b5640bd7e1a3c3

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {return string}
+"Hello!".toUpperCase()
+function shout(string) {return string.toUpperCase()}
+
+function whisper(string) {return string}
+"Hello.".toLowerCase()
+function whisper(string) {return string.toLowerCase()}
+
+function logShout(string) {console.log(string)}
+"Hello!".toUpperCase()
+function logShout(string) {console.log(string.toUpperCase())}
+
+function logWhisper(string) {console.log(string)}
+"Hello!".toLowerCase()
+function logWhisper(string) {console.log(string.toLowerCase())}
+
+function sayHiToHeadphonedRoommate(string) {return string}
+"I can't hear you!".toLowerCase()
+function sayHiToHeadphonedRoommate(string) {return string.toLowerCase()}
